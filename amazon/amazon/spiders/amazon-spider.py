7863ce3ac139ec8933449b3b8473ee7c2d8de49c
Shrubin <shrubin93.skn@gmail.com>
2016-02-08 14:45:14 +0530

    amazon fixed

--- amazon/amazon/spiders/amazon-spider.py
+++ amazon/amazon/spiders/amazon-spider.py
@@ -12,14 +12,13 @@ class DmozSpider(CrawlSpider):
     start_urls = ["http://www.amazon.com/s?ie=UTF8&page=1&rh=n%3A2407749011%2Ck%3Amobile%20phones"
     ]
     
-    rules = (
-        Rule(LxmlLinkExtractor(allow=(), restrict_xpaths=('//a[@id="pagnNextLink"]',)), callback='parse_page', follow=True),
-    )
-
-    def parse_page(self, response):
+    def parse(self, response):
+        for next_url in response.xpath('//*[@id="pagnNextLink"]/@href'):
+            url_complete = response.urljoin(next_url.extract())
         for href in response.xpath('//*[@class="s-result-item  celwidget "]/div/div[2]/div[1]/a/@href'):
             full_url = response.urljoin(href.extract())
-            return(scrapy.Request(full_url, callback=self.parse_items))
+            yield scrapy.Request(full_url, callback=self.parse_items)
+        yield scrapy.Request(url_complete, callback=self.parse)    
 
     def parse_items(self, response):
         items = AmazonItem()
@@ -27,6 +26,6 @@ class DmozSpider(CrawlSpider):
         items['features'] = response.xpath('.//*[@id="feature-bullets"]/ul//li/span/text()').extract()
         items['product_url'] = response.url
         items['image_urls'] = response.xpath('.//*[@id="landingImage"]/@src').extract()
-        return(items)
+        yield items
        
         
