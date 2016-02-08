65ef6c68d44256049453e8000cbd8aef1fa69b59
Shrubin <shrubin93.skn@gmail.com>
2016-02-08 23:35:34 +0530

    made changes to walmart-scraper, needs some more work

--- walmart on bs4/walmart_scraper.py
+++ walmart on bs4/walmart_scraper.py
@@ -3,57 +3,69 @@ import urllib
 import requests     
 import os
 import urllib2
-
-r = urllib.urlopen('http://www.walmart.com/search/?query=mobile%20phones&cat_id=3944_542371_1073085').read()
-soupthis = BeautifulSoup(r)
+import json
 
 products = {}
 
-product_divs = soupthis.find_all("div", class_="js-tile js-tile-landscape tile-landscape")
+def _crawl_():
+    r = urllib.urlopen('http://www.walmart.com/search/?query=mobile%20phones&cat_id=3944_542371_1073085').read()
+    soupthis = BeautifulSoup(r)
+    while soupthis:
+        product_divs = soupthis.find_all("div", class_="js-tile js-tile-landscape tile-landscape")        
+        
+        for product_div in product_divs:
+            product = {}
+            text = product_div.div.h4.a.get_text()
+            link = product_div.a['href']
+            link = 'http://walmart.com' + link
+            product['product_link'] = link
+            print product['product_link']
+            products.setdefault(text,[]).append(product)
 
-for product_div in product_divs:
-    product = {}
-    text = product_div.div.h4.a.get_text()
-    link = product_div.a['href']
-    link = 'http://walmart.com' + link
-    product['product_link'] = link
-    print product['product_link']
-    products.setdefault(text,[]).append(product)
+        for productName in products:
+            values = products[productName]
+            print values
+            child = values[0]
+            product = {}
+            link = child['product_link']
+            print link
+            r = requests.get(link)
+            data = r.text
+            soup = BeautifulSoup(data)
+            div_features = soup.find_all("div", class_="js-ellipsis module")
+            product['features'] = div_features[0].get_text()
+            products[productName].append(product)  
     
-
-for productName in products:
-    values = products[productName]
-    print values
-    child = values[0]
-    product = {}
-    link = child['product_link']
-    print link
-    r = requests.get(link)
-    data = r.text
-    soup = BeautifulSoup(data)
-    div_features = soup.find_all("div", class_="js-ellipsis module")
-    product['features'] = div_features[0].get_text()
-    products[productName].append(product)  
-
-for product_div in product_divs:
-    product = {}
-    print product_div.a.img['data-default-image']
-    product['img_src'] = product_div.a.img['data-default-image']
-    print product['img_src']
-    products[product_div.div.h4.a.get_text()].append(product)
+        for product_div in product_divs:
+            product = {}
+            print product_div.a.img['data-default-image']
+            product['img_src'] = product_div.a.img['data-default-image']
+            print product['img_src']
+            products[product_div.div.h4.a.get_text()].append(product)
     
-
-for productName in products:
-    values = products[productName]
-    imgUrl_dict = values[2]
-    print imgUrl_dict
-    imgUrl = imgUrl_dict['img_src']
-    url_img = os.path.basename(imgUrl)
-    url_dir = ""
-    url_dir = "C:\walmart\\" + url_img
-    urllib.urlretrieve(imgUrl, url_dir)
-    product = {}
-    product['img_local'] = url_dir
-    products[productName].append(product)
+        for productName in products:
+            values = products[productName]
+            imgUrl_dict = values[2]
+            print imgUrl_dict
+            imgUrl = imgUrl_dict['img_src']
+            url_img = os.path.basename(imgUrl)
+            url_dir = ""
+            url_dir = "C:\walmart\\" + url_img
+            urllib.urlretrieve(imgUrl, url_dir)
+            product = {}
+            product['img_local'] = url_dir
+            products[productName].append(product)
     
-products
+        next_button = soupthis.find_all("a", class_="paginator-btn paginator-btn-next")
+        soupthis = []
+        if next_button:
+            next_ = next_button[0]['href']
+            next_link = "http://www.walmart.com/" + next_
+            r = requests.get(next_link)
+            data = r.text
+            soupthis = BeautifulSoup(data)
+    with open("wallmart_products.json", "w") as writeJSON:
+        json.dump(products, writeJSON)        
+    
+    
+_crawl_()
